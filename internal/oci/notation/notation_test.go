a4c93d9ffd3820a18fbe5b4df2eceaac79699e1f
Jason <jagoodse@microsoft.com>
2024-03-13 20:09:43 +1100

    test(notation): add tests to check trust policy cleaner works as expected
    Signed-off-by: Jason <jagoodse@microsoft.com>

--- internal/oci/notation/notation_test.go
+++ internal/oci/notation/notation_test.go
@@ -21,9 +21,11 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/go-logr/logr"
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/notaryproject/notation-go/verifier/trustpolicy"
+	. "github.com/onsi/gomega"
 )
 
 func TestOptions(t *testing.T) {
@@ -179,6 +181,176 @@ func TestOptions(t *testing.T) {
 	}
 }
 
+func TestCleanTrustPolicy(t *testing.T) {
+	g := NewWithT(t)
+
+	testCases := []struct {
+		name       string
+		policy     []trustpolicy.TrustPolicy
+		want       *trustpolicy.Document
+		logMessage string
+	}{
+		{
+			name: "no trust policy",
+			want: nil,
+		},
+		{
+			name: "trust policy verification level set to strict and should not be cleaned",
+			policy: []trustpolicy.TrustPolicy{{
+				Name:                  "test-statement-name",
+				RegistryScopes:        []string{"*"},
+				SignatureVerification: trustpolicy.SignatureVerification{VerificationLevel: "strict"},
+				TrustStores:           []string{"test"},
+				TrustedIdentities:     nil,
+			}},
+			want: &trustpolicy.Document{
+				Version: "1.0",
+				TrustPolicies: []trustpolicy.TrustPolicy{{
+					Name:                  "test-statement-name",
+					RegistryScopes:        []string{"*"},
+					SignatureVerification: trustpolicy.SignatureVerification{VerificationLevel: "strict"},
+					TrustStores:           []string{"test"},
+					TrustedIdentities:     nil,
+				}},
+			},
+		},
+		{
+			name: "trust policy with multiple policies and should not be cleaned",
+			policy: []trustpolicy.TrustPolicy{
+				{
+					Name:                  "test-statement-name",
+					RegistryScopes:        []string{"*"},
+					SignatureVerification: trustpolicy.SignatureVerification{VerificationLevel: "strict"},
+					TrustStores:           []string{"test"},
+					TrustedIdentities:     []string{"x509.subject:CN=Notation Test Root,O=Notary,L=Seattle,ST=WA,C=US"},
+				},
+				{
+					Name:                  "test-statement-name-2",
+					RegistryScopes:        []string{"example.com/podInfo"},
+					SignatureVerification: trustpolicy.SignatureVerification{VerificationLevel: "strict"},
+					TrustStores:           []string{"test"},
+					TrustedIdentities:     nil,
+				},
+			},
+			want: &trustpolicy.Document{
+				Version: "1.0",
+				TrustPolicies: []trustpolicy.TrustPolicy{
+					{
+						Name:                  "test-statement-name",
+						RegistryScopes:        []string{"*"},
+						SignatureVerification: trustpolicy.SignatureVerification{VerificationLevel: "strict"},
+						TrustStores:           []string{"test"},
+						TrustedIdentities:     []string{"x509.subject:CN=Notation Test Root,O=Notary,L=Seattle,ST=WA,C=US"},
+					},
+					{
+						Name:                  "test-statement-name-2",
+						RegistryScopes:        []string{"example.com/podInfo"},
+						SignatureVerification: trustpolicy.SignatureVerification{VerificationLevel: "strict"},
+						TrustStores:           []string{"test"},
+						TrustedIdentities:     nil,
+					},
+				},
+			},
+		},
+		{
+			name: "trust policy verification level skip should be cleaned",
+			policy: []trustpolicy.TrustPolicy{
+				{
+					Name:                  "test-statement-name",
+					RegistryScopes:        []string{"*"},
+					SignatureVerification: trustpolicy.SignatureVerification{VerificationLevel: "skip"},
+					TrustStores:           []string{"test"},
+					TrustedIdentities:     []string{"x509.subject:CN=Notation Test Root,O=Notary,L=Seattle,ST=WA,C=US"},
+				},
+			},
+			want: &trustpolicy.Document{
+				Version: "1.0",
+				TrustPolicies: []trustpolicy.TrustPolicy{
+					{
+						Name:                  "test-statement-name",
+						RegistryScopes:        []string{"*"},
+						SignatureVerification: trustpolicy.SignatureVerification{VerificationLevel: "skip"},
+						TrustStores:           []string{},
+						TrustedIdentities:     []string{},
+					},
+				},
+			},
+			logMessage: "warning: trust policy statement 'test-statement-name' is set to skip signature verification but configured with trust stores and/or trusted identities. Removing trust stores and trusted identities",
+		},
+		{
+			name: "trust policy with multiple policies and mixture of verification levels including ship",
+			policy: []trustpolicy.TrustPolicy{
+				{
+					Name:                  "test-statement-name",
+					RegistryScopes:        []string{"*"},
+					SignatureVerification: trustpolicy.SignatureVerification{VerificationLevel: "strict"},
+					TrustStores:           []string{"test"},
+					TrustedIdentities:     []string{"x509.subject:CN=Notation Test Root,O=Notary,L=Seattle,ST=WA,C=US"},
+				},
+				{
+					Name:                  "test-statement-name-2",
+					RegistryScopes:        []string{"example.com/podInfo"},
+					SignatureVerification: trustpolicy.SignatureVerification{VerificationLevel: "skip"},
+					TrustStores:           []string{"test"},
+					TrustedIdentities:     []string{"x509.subject:CN=Notation Test Root,O=Notary,L=Seattle,ST=WA,C=US"},
+				},
+			},
+			want: &trustpolicy.Document{
+				Version: "1.0",
+				TrustPolicies: []trustpolicy.TrustPolicy{
+					{
+						Name:                  "test-statement-name",
+						RegistryScopes:        []string{"*"},
+						SignatureVerification: trustpolicy.SignatureVerification{VerificationLevel: "strict"},
+						TrustStores:           []string{"test"},
+						TrustedIdentities:     []string{"x509.subject:CN=Notation Test Root,O=Notary,L=Seattle,ST=WA,C=US"},
+					},
+					{
+						Name:                  "test-statement-name-2",
+						RegistryScopes:        []string{"example.com/podInfo"},
+						SignatureVerification: trustpolicy.SignatureVerification{VerificationLevel: "skip"},
+						TrustStores:           []string{},
+						TrustedIdentities:     []string{},
+					},
+				},
+			},
+			logMessage: "warning: trust policy statement 'test-statement-name-2' is set to skip signature verification but configured with trust stores and/or trusted identities. Removing trust stores and trusted identities",
+		},
+	}
+
+	// Run the test cases
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			l := &testLogger{[]string{}, logr.RuntimeInfo{CallDepth: 1}}
+			logger := logr.New(l)
+
+			if tc.want == nil {
+				cleanedPolicy := cleanTrustPolicy(nil, logger)
+				if !reflect.DeepEqual(cleanedPolicy, tc.want) {
+					t.Errorf("got %#v, want %#v", cleanedPolicy, tc.want)
+				}
+				return
+			}
+
+			policy := trustpolicy.Document{
+				Version:       "1.0",
+				TrustPolicies: tc.policy,
+			}
+
+			cleanedPolicy := cleanTrustPolicy(&policy, logger)
+
+			if !reflect.DeepEqual(cleanedPolicy, tc.want) {
+				t.Errorf("got %#v, want %#v", cleanedPolicy, tc.want)
+			}
+
+			if tc.logMessage != "" {
+				g.Expect(len(l.Output)).Should(Equal(1))
+				g.Expect(l.Output[0]).Should(Equal(tc.logMessage))
+			}
+		})
+	}
+}
+
 func dummyPolicyDocument() (policyDoc *trustpolicy.Document) {
 	policyDoc = &trustpolicy.Document{
 		Version:       "1.0",
@@ -196,4 +368,38 @@ func dummyPolicyStatement() (policyStatement trustpolicy.TrustPolicy) {
 		TrustedIdentities:     []string{"x509.subject:CN=Notation Test Root,O=Notary,L=Seattle,ST=WA,C=US"},
 	}
 	return
+}
+
+// mocking LogSink to capture log messages. Source: https://stackoverflow.com/a/71425740
+type testLogger struct {
+	Output []string
+	r      logr.RuntimeInfo
+}
+
+func (t *testLogger) doLog(msg string) {
+	t.Output = append(t.Output, msg)
+}
+
+func (t *testLogger) Init(info logr.RuntimeInfo) {
+	t.r = info
+}
+
+func (t *testLogger) Enabled(level int) bool {
+	return true
+}
+
+func (t *testLogger) Info(level int, msg string, keysAndValues ...interface{}) {
+	t.doLog(msg)
+}
+
+func (t *testLogger) Error(err error, msg string, keysAndValues ...interface{}) {
+	t.doLog(msg)
+}
+
+func (t *testLogger) WithValues(keysAndValues ...interface{}) logr.LogSink {
+	return t
+}
+
+func (t *testLogger) WithName(name string) logr.LogSink {
+	return t
 }
